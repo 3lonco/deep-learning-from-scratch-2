b4c7e52822e3fafcb9e4d10228e5483b8f5883a8
Jun Kubota <kinkado115@gmail.com>
2023-03-09 10:24:45 +0000

    ｌ

--- myself/rnn1.py
+++ myself/rnn1.py
@@ -1,7 +1,10 @@
+import numpy as np
+
+
 class RNN:
     def __init__(self, Wx, Wh, b):
         self.params = [Wx, Wh, b]
-        self.grad = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
         self.cache = None
 
     def forward(self, x, h_prev):
@@ -11,3 +14,20 @@ class RNN:
 
         self.cache = (x, h_prev, h_next)
         return h_next
+
+    def backward(self, dh_next):
+        Wx, Wh, b = self.params
+        x, h_prev, h_next = self.cache
+
+        dt = dh_next * (1 - h_next**2)
+        db = np.sum(dt, axis=0)
+        dWh = np.dot(h_prev.T, dt)
+        dh_prev = np.dot(dt, Wh.T)
+        dWx = np.dot(x.T, dt)
+        dx = np.dot(dt, Wx.T)
+
+        self.grads[0][...] = dWx
+        self.grads[1][...] = dWh
+        self.grads[2][...] = db
+
+        return dx, dh_prev
